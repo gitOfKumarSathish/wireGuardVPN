9e6bd1733899b9e1dac0d77c0f2441813465d10b
Sathish <sathish@alethea.in>
2025-02-07 12:58:51 +0530

    Add NotFound page and update routing; enhance Home component with navigation buttons

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import Home from './auth/Home';
 import NavigationDrawer from './components/NavigationDrawer';
 import Dashboard from './pages/Dashboard';
 import Help from './pages/Help';
+import NotFound from './pages/NotFound';
 import PeerDetails from './pages/PeerDetails';
 import Peers from './pages/Peers';
 import Settings from './pages/Settings';
@@ -22,6 +23,7 @@ function App() {
         <Route path="/peers/:publicKey" element={<PeerDetails />} />
         <Route path="/settings" element={<Settings />} />
         <Route path="/help" element={<Help />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
